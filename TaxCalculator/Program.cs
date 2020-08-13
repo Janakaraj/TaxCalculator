0af464b9af9aac39cf20c6932fbd9e14766fc2bf
Janakaraj <janak@promactinfo.com>
2020-08-13 15:16:25 +0530

    First Commit

---
+++ TaxCalculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int age = 0;
+                double salary = 0;
+                bool isAgeNegative = true;
+                bool isSalaryNegative = true;
+                while (isAgeNegative)
+                {
+                    Console.WriteLine("Please enter your age");
+                    age = Convert.ToInt32(Console.ReadLine());
+                    if (age >= 0) {
+                        isAgeNegative = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Age cannot be negative");
+                    }
+                }
+                while (isSalaryNegative)
+                {
+                    Console.WriteLine("Please enter your anual salary");
+                    salary = Convert.ToDouble(Console.ReadLine());
+                    if (salary >= 0)
+                    {
+                        isSalaryNegative = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Salary cannot be negative");
+                    }
+                }
+
+                Console.WriteLine();
+                TaxCalculator t1 = new TaxCalculator(age, salary);
+                Console.WriteLine("Calculated tax : Rs {0}", t1.CalculateTax());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ TaxCalculator/TaxCalculator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxCalculator
+{
+    class TaxCalculator
+    {
+        private double _taxPercentageSlab1 = 5.0;
+        private double _taxPercentageSlab2 = 20.0;
+        private double _taxPercentageSlab3 = 30.0;
+        private double Salary { get; set; }
+        private int Age { get; set; }
+        public TaxCalculator(int age, double salary)
+        {
+            this.Salary = salary;
+            this.Age = age;
+        }
+        public double CalculateTax()
+        {
+            if (this.Age <= 60)
+            {
+                if (this.Salary <= 250000)
+                {
+                    return 0;
+                }
+                else if (this.Salary > 250000 && this.Salary <= 500000)
+                {
+                    return (this.Salary * _taxPercentageSlab1) / 100;
+                }
+                else if (this.Salary > 500000 && this.Salary <= 1000000)
+                {
+                    return (((this.Salary - 500000) * _taxPercentageSlab2) / 100) + 12500;
+                }
+                else if (this.Salary > 1000000)
+                {
+                    return (((this.Salary - 1000000) * _taxPercentageSlab3) / 100) + 112500;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else if(this.Age > 60 && this.Age <= 80)
+            {
+                if (this.Salary <= 300000)
+                {
+                    return 0;
+                }
+                else if (this.Salary > 300000 && this.Salary <= 500000)
+                {
+                    return (this.Salary * _taxPercentageSlab1) / 100;
+                }
+                else if (this.Salary > 500000 && this.Salary <= 1000000)
+                {
+                    return (((this.Salary - 500000) * _taxPercentageSlab2) / 100) + 10000;
+                }
+                else if (this.Salary > 1000000)
+                {
+                    return (((this.Salary - 1000000) * _taxPercentageSlab3) / 100) + 110000;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else if (this.Age > 80)
+            {
+                if (this.Salary <= 500000)
+                {
+                    return 0;
+                }
+                else if (this.Salary > 500000 && this.Salary <= 1000000)
+                {
+                    return ((this.Salary * _taxPercentageSlab2) / 100);
+                }
+                else if (this.Salary > 1000000)
+                {
+                    return (((this.Salary - 1000000) * _taxPercentageSlab3) / 100) + 100000;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
